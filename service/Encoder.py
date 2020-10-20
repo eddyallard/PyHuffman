5f2500726655faa0d6571aebe316514f79754739
Bepux <lavoie.felix@hotmail.com>
2020-10-20 11:36:19 -0400

    ceac

--- service/Encoder.py
+++ service/Encoder.py
@@ -1,5 +1,6 @@
 from adt.list.ArrayList import ArrayList
 from adt.tree.HuffTree import HuffTree
+from model.HuffData import HuffData
 from model.HuffTable import HuffTable
 from service.BinaryHelper import BinaryHelper
 from service.FileHelper import FileHelper
@@ -17,7 +18,7 @@ class Encoder:
         huff = HuffTree(file_helper.fetch_symbols())
         original_file = open(self.path + self.filename+".txt", "r")
         compressed_file = open(self.path + self.filename + self.extension, "ab")
-        compressed_file_header = open(self.path + "binboi.bin", "w")
+        compressed_file_header = open(self.path + self.filename + self.extension, "w")
         huff_data = huff.get_huff_list()
         binary = ""
         for symbol in original_file.read():
@@ -49,4 +50,37 @@ class Encoder:
         return table
 
     def decode(self):
-        pass
+        frequency, table,offset = self.fetch_header_from_file()
+
+
+
+    def fetch_header_from_file(self):
+        compressed_file = open(self.path + self.filename + self.extension, "r", errors='ignore')
+        start_flag = True
+        end_char = ""
+        table = ArrayList()
+        total_frequency = ""
+        huffchar = ""
+        huffbin = ""
+        offset = 0
+        for symbol in compressed_file.read():
+            offset += 1
+            print("benis")
+            if symbol.isdigit() and start_flag:
+                total_frequency += symbol
+            elif symbol.isascii() and symbol.isdigit() is not True and start_flag:
+                end_char += symbol
+                huffchar = symbol
+                end_char = symbol
+                start_flag = False
+            elif symbol.isdigit():
+                huffbin += symbol
+            elif symbol.isascii() and symbol.isdigit() is not True and symbol is not end_char:
+                table.add_back(HuffData(huffchar, None, huffbin))
+                huffchar = symbol
+                huffbin = ""
+            elif symbol is end_char:
+                break
+        print(total_frequency)
+
+        return total_frequency, table, offset
