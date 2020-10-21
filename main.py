ccc229ebab81cad8fff888d039ee6610fa33c671
bepux <lavoie.felix@hotmail.com>
2020-10-21 15:52:31 -0400

    basic_cli_linux

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from service.Encoder import Encoder
+import argparse
 
-felix = "C:\\Users\\Bepix\\Desktop\\realboi\\src\\dataset\\"
-eddy = "/home/eddy/Documents/Coding/PycharmProjects/PyHuffman/dataset/"
-encode = Encoder("beenus", eddy)
-encode.encode()
-print(f"{encode.decode()}")
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="The path of the file", type=str)
+    parser.add_argument("mode", help="E : Encoding mode | D : Decoding mode", choices=['E', 'D'])
+    args = parser.parse_args()
+    path, filename = args.path.rsplit('/', 1)
+    path += "/"
+    filename = filename.split('.')[0]
+    if args.mode == 'E':
+        encode = Encoder(filename,path)
+        print("Encoding...")
+        encode.encode()
+        print ("Encoding was successful!")
+    else:
+        print(filename)
+        print(path)
+        encode = Encoder(filename, path)
+        print("Decoding...")
+        print(encode.decode())
+        print("Decoding was successful!")
+
+
+Main()
