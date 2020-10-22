1c1b8c69cd6f700719f1723b56b2df203df2ace0
bepix <bepix@pop-os.localdomain>
2020-10-21 22:56:49 -0400

    Windows cli -To test

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from service.Encoder import Encoder
 import argparse
 
-
+def OsChecker(path):
+    if "/" in path:
+        return True
+    else:
+        return False
 def Main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="The path of the file", type=str)
     parser.add_argument("mode", help="E : Encoding mode | D : Decoding mode", choices=['E', 'D'])
     args = parser.parse_args()
-    path, filename = args.path.rsplit('/', 1)
-    path += "/"
-    filename = filename.split('.')[0]
+    if OsChecker(args.path):
+        path, filename = args.path.rsplit("/", 1)
+        path += "/"
+        filename = filename.split('.')[0]
+    else:
+        path, filename = args.path.rsplit("\\", 1)
+        path += "\\"
+        filename = filename.split('.')[0]
     if args.mode == 'E':
         encode = Encoder(filename,path)
         print("Encoding...")
